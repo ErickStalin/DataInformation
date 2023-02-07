4a2cf9f00092362cf27c633934fafec3c636e96b
Erick <erick.santillan@epn.edu.ec>
2023-02-07 11:56:04 -0500

    Changed in the code

--- src/form1.java
+++ src/form1.java
@@ -1,66 +1,64 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 public class form1 {
     PreparedStatement ps;
-    private JTextField textField4;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JTextField celular;
+    private JTextField nombre;
+    private JTextField id;
+    private JTextField correo;
     private JButton GUARDARButton;
     private JPanel p1;
+    public static Connection getConecction() {
+        Connection cn = null;
+        String base = "Estudiantes"; //Nombre de la BD
+        String url = "jdbc:mysql://localhost:3306/" + base; //Direccion, puerto y nombre BD
+        String user = "root"; //Usuario
+        String pass = "0986167219"; //Contraseña
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection(url,user,pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+        return cn;
+    }
     public form1() {
-
         GUARDARButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Connection con;
-
-                try{
-                    con = getConecction();
-                    ps = con.prepareStatement("INSERT INTO datosPersona(IdPerson,NomPerson,CelPerson,MailPerson) VALUES(?,?,?,?)");
-                    ps.setString(1,textField2.getText());
-                    ps.setString(2,textField1.getText());
-                    ps.setString(3,textField4.getText());
-                    ps.setString(4,textField3.getText());
+                Connection cn;
+                try {
+                    cn = getConecction();
+                    ps = cn.prepareStatement("INSERT INTO datosEstudiantes (Id, Nombre, celular, email) values (?,?,?,?)");
+                    ps.setString(1, id.getText());
+                    ps.setString(2, nombre.getText());
+                    ps.setString(3, celular.getText());
+                    ps.setString(4, correo.getText());
                     System.out.println(ps);
                     int res = ps.executeUpdate();
-                    if(res > 0){
+                    if (res > 0) {
                         JOptionPane.showMessageDialog(null,"Persona guardada");
-                    }
-                    else{
+                    } else {
                         JOptionPane.showMessageDialog(null,"Error");
                     }
-                    con.close();
+                    cn.close();
                 } catch (Exception ex) {
                     throw new RuntimeException(ex);
                 }
-
             }
         });
     }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("form1");
-        frame.setContentPane(new form1().p1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-    public static Connection getConecction() {
-        Connection con = null;
-        String base= "datos";
-        String url= "jdbc:mysql://localhost"+base;
-        String user = "root";
-        String password = "0986167219";
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url,user,password);
-        }catch (ClassNotFoundException | SQLException e){
-            System.err.println(e);
-        }
-        return con;
+        JFrame fr = new JFrame("form1");
+        fr.setContentPane(new form1().p1);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.pack();
+        fr.setVisible(true);
+        fr.setResizable(false);
+        fr.setLocationRelativeTo(null);
     }
 
 }
